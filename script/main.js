76ce8c1f5138008dd273fd1754fb1d3f8bf9312f
Daniil-Mb <sedfetiop@gmail.com>
2023-07-09 18:58:40 +0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,14 @@
+// fixed Header
+
+// const header = document.querySelector('.header__header');
+
+
+// window.addEventListener('scroll', () => {
+// let scrollsY = window.scrollY;
+
+// if (scrollsY >= 100) {
+// header.classList.add('fixed')
+// } else {
+// header.classList.remove('fixed')
+// }
+// });
